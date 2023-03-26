533b9665a4292d56cf9cb5dc6de3070bf46e88e0
Areebey <areebrafiq125@gmail.com>
2023-03-27 03:49:42 +0500

    make nav buttons wth of bootstrap

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,10 +5,22 @@ import "./Navbar.css"
 const Navbar = () => {
   return (
     <nav>
-        navbar
         <ul>
+            <li><button data-bs-toggle="dropdown" aria-expanded="true"  className="dropdown-toggle" >Features
+            <ul className='dropdown-menu' >
+                <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente quasi ratione optio quod non excepturi molestiae sit a dignissimos ullam!</li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente quasi ratione optio quod non excepturi molestiae sit a dignissimos ullam!</li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente quasi ratione optio quod non excepturi molestiae sit a dignissimos ullam!</li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente quasi ratione optio quod non excepturi molestiae sit a dignissimos ullam!</li>
+            </ul>
+            </button></li>
+            <li><button className="dropdown-toggle">Solutions</button></li>
+            <li><button className="dropdown-toggle">Plans</button></li>
+            <li><button >Pricing</button></li>
+            <li><button className="dropdown-toggle">Resources</button></li>
         </ul>
     </nav>
+
   )
 }
 
