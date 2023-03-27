8e89dd707df3875150c2f9717e22d9369dc91636
Talha-Ali150 <syed.talha.ali.150@gmail.com>
2023-03-27 18:40:38 +0500

    section 1 unresponsive

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import Heading1 from './Components/Heading1';
 
 function App() {
   return (
     <div className="App">
     <Navbar/>
+    <Heading1 />
     </div>
   );
 }
