8369719d837861766cc9cfb2cf21d94cefa48b5b
Areebey <areebrafiq125@gmail.com>
2023-03-28 18:17:42 +0500

    little changes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React  from 'react'
 import logo from "../../images/Trello_logo.svg.png"
 import "./Navbar.css"
 
@@ -8,8 +8,9 @@ const Navbar = () => {
     <nav>
         <ul>
             <img src={logo} alt="trello-logo" />
-            <li><button data-bs-toggle="dropdown" aria-expanded="true"  className="dropdown-toggle" >Features
-            <ul className='dropdown-menu' >
+            <li><button data-bs-toggle="dropdown" 
+              className="dropdown-toggle" >Features
+            <div className='dropdown-menu' >
              <div className='content-container'>
               <div className='container-1' >
               <h3>Explore the features that help your team succeed</h3> 
@@ -20,17 +21,15 @@ const Navbar = () => {
               <div><h2>Power-Ups</h2></div>
               <div><h2>Templates</h2></div>
               <div><h2>Integration</h2></div>
-
               </div>
+
               <div className='container-2'>
                 <h2>Meet Trello</h2>
                 </div>
-
+             </div> 
              </div>
-               
-                {/* <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente quasi ratione optio quod non excepturi molestiae sit a dignissimos ullam!</li> */}
-            </ul>
             </button></li>
+
             <li><button className="dropdown-toggle">Solutions</button></li>
             <li><button className="dropdown-toggle">Plans</button></li>
             <li><button >Pricing</button></li>
