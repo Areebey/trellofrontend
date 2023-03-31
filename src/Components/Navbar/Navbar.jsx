bae64c7946540b0f47e10d3cc2cfd0d7f9cd42d9
Areebey <areebrafiq125@gmail.com>
2023-04-01 03:34:49 +0500

    working on features btn

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,47 +1,94 @@
-import React  from 'react'
+import React,{useState,useEffect,useRef} from 'react'
 import logo from "../../images/Trello_logo.svg.png"
 import "./Navbar.css"
 
 
 const Navbar = () => {
+  const [isOpen,setIsOpen]=useState(false)
+  const dropdownRef = useRef(null);
+
+  const toggleBtn=()=>{
+    setIsOpen(!isOpen)
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [dropdownRef]);
+
   return (
     <nav>
         <ul>
-            <img src={logo} alt="trello-logo" />
-            <li><button data-bs-toggle="dropdown" 
-              className="dropdown-toggle" >Features
-            <div className='dropdown-menu' >
+          <li>
+           <img src={logo} alt="trello-logo" /></li>
+            <li><button  className="Features" onClick={toggleBtn} >Features
+            <div className='dropdown-menu' ref={dropdownRef} >
+              {isOpen &&(
              <div className='content-container'>
               <div className='sub-container-1' >
-                <p>Explore the features that help your team succeed</p>
-              {/* <h4>Explore the features that help your team succeed</h4>  */}
-
+                <h2>Explore the features that help your team succeed</h2>
                 <div className='container-1'>
               <div className='views'>
                <h4>Views</h4>
-               <p>View your team's project from every angle</p>
+               <p>View your team's project from every angle.</p>
               </div>
-              <div><h2>Automation</h2></div>
-              <div><h2>Power-Ups</h2></div>
-              <div><h2>Templates</h2></div>
-              <div><h2>Integration</h2></div>
+              
+              <div className='automation'>
+                <h4>Automation</h4>
+                <p>Automate tasks and workflows with Butler automation.</p>
+                </div>
+              <div className='power-ups'>
+                <h4>Power-Ups</h4>
+                <p>Power up your teams by linking their favorite tools with Trello plugins.</p>
+                </div>
+                <div className='container-1Sec-2'>
+              <div className='templates'>
+                <h4>Templates</h4>
+                <p>Give your team a blueprint for success with easy-to-use 
+                  templates from industry leaders and the Trello community.</p>
+                </div>
+              <div className='integration'>
+                <h4>Integration</h4>
+                <p>Find the apps your team is already using or discover 
+                  new ways to get work done in Trello.</p>
+                </div>
+                </div>
               </div>
               </div>
 
               <div className='container-2'>
-                <h2>Meet Trello</h2>
+                {/* <div className='meet-trello'> */}
+                <h3>Meet Trello</h3>
+                {/* </div> */}
+                <div className='content-trello'>
+                  <p>Trello makes it easy for your team to get work done. No matter the project, workflow, or type of team, 
+                    Trello can help keep things organized. It’s simple –  sign-up, 
+                    create a board, and you’re off! Productivity awaits.</p>
+                </div>
+                <div className='trello-btn'>
+                  <span>Check out Trello</span>
+                </div>
                 </div>
              </div> 
+             )}
              </div>
             </button></li>
 
-            <li><button className="dropdown-toggle">Solutions</button></li>
-            <li><button className="dropdown-toggle">Plans</button></li>
+            <li><button>Solutions</button></li>
+            <li><button>Plans</button></li>
             <li><button >Pricing</button></li>
-            <li><button className="dropdown-toggle">Resources</button></li>
+            <li><button>Resources</button></li>
+
         <button className="login-btn">Login</button>
-        </ul>
         <button className="get-trello-btn">Get Trello For Free</button>
+        </ul>
     </nav>
 
   )
