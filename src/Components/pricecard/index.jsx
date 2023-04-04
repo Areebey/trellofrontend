edf49d4c491e731380655c586e89f375827c4728
sarmad505 <sarmadbaig505@gmail.com>
2023-04-04 06:01:33 +0500

    css

--- src/Components/pricecard/index.jsx
+++ src/Components/pricecard/index.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import './index.css'
 import price from "../../images/pricecard.PNG"
-//import img from "../../images/slider1.PNG"
+import img from "../../images/slider1.PNG"
 
 function Card() {
   return (
-    <div>
-        <h1>
+    <div className='container'>
+        <h1 className='head'>
         Trello priced your way
         </h1>
         
-        <p>Trusted by millions, Trello powers teams all around the world.</p>
-        <button>Compare Plans</button>
+        <p className='para'>Trusted by millions, Trello powers teams all around the world.</p>
+        <button className='btn' type='submit'>Compare Plans</button>
         <div>
-        <image className='img' src={price} alt='price card show'/>;
+        <img className='img' src={price} alt='price card show'/>
         </div>
         
     </div>
